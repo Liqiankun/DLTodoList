52e80e4bf43ebe32e617c10b8ac41f77cadf0783
David <qiankunli@yahoo.com>
2018-02-10 21:02:23 +0800

    done

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -14,6 +14,10 @@ $("input[type='text']").keypress(function (event) {
   if (event.which === 13) {
     var inputValue = $(this).val()
     $(this).val('')
-    $('ul').append(`<li><span>X </span>${inputValue}</li>`)
+    $('ul').append(`<li><span><i class="fas fa-trash-alt"></i></span>${inputValue}</li>`)
   }
+})
+
+$('.fa-plus').click(function () {
+  $("input[type='text']").fadeToggle()
 })
